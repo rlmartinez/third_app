2cb2fcbedec81d8cc5ba894476823c342cab92e8
rlmartinez <rlm233@msn.com>
2014-06-09 17:51:36 -0500

    Initialize Repository - RLM

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThirdApp::Application.config.secret_key_base = 'e7fcc334aef9d07a03aa926b8faccd939514b4776695b4c2676005fca47208efbb84f185400ec7b3b9bf5ddb05ee845818068a07eaae612ddb69194aee50ffad'
